35761aca89111bbcbb1fe3cf349c4d9fb997a961
joseluza <u202213404@upc.edu.pe>
2025-06-20 11:31:47 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/logisticmaster/backendandroid/BackendAndroidApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.logisticmaster.backendandroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAndroidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAndroidApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/logisticmaster/backendandroid/BackendAndroidApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.logisticmaster.backendandroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAndroidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
