3bb932496519c311b4d8d3cd5ebc534909e71e3a
joseluza <u202213404@upc.edu.pe>
2025-07-10 22:19:19 -0500

    feat: CorsConfig

---
+++ src/main/java/pe/edu/upc/logisticmaster/backendandroid/backend/configuration/CorsConfig.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.logisticmaster.backendandroid.backend.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Configura CORS para todos los endpoints y permite cualquier origen
+        registry.addMapping("/**") // Aplica CORS a todos los endpoints
+                .allowedOrigins("*") // Permite solicitudes desde cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowedHeaders("*") // Permite todas las cabeceras
+                .allowCredentials(true); // Permite enviar credenciales (como cookies)
+    }
+}
